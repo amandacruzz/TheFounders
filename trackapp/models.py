9ab4dc61a686ef531de978b8e0222acc924ebf4e
Chrisclone <cpestano520@gmail.com>
2022-12-05 22:09:06 -0500

    basic image upload

--- trackapp/models.py
+++ trackapp/models.py
@@ -28,7 +28,7 @@ class lostItem(models.Model):
 	title = models.CharField(max_length=1000, null=True)
 	location = models.CharField(max_length=1000, null=True)
 	description = models.CharField(max_length=1000, null=True)
-	image = models.ImageField(upload_to = "images/")
+	image = models.ImageField(upload_to = "static/img/")
 
 class Acc_Positions(models.Model):
 	username_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
